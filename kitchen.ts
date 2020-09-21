beb5077acba297ac66ad5228ab1d019edec229d6
Alloys Mila <mofax@users.noreply.github.com>
2020-09-21 09:43:30 +0300

    Introduce Kitchen.prepareRecipes and Kitchen constructor
    This static method takes in the result of Kitchen.loadIngredients
    together with a second param that is a list of recipes.
    
    It will then loop through all recipes and compared the ingredient
    list with our list on available(unexpired) ingredients.
    
    A recipe is marked as available if all ingredients are available
    otherwise it is marked as unavailable.
    
    This function will then return two arrays containing available and
    unavailable recipes.
    
    We provide a constructor that stitches up Kitchen.loadIngredients
    and Kitchen.prepareRecipes.
    
    We provide a class method `recommendARecipe` that returns the available
    recipes.

--- kitchen.ts
+++ kitchen.ts
@@ -4,7 +4,28 @@ export interface I_Ingredient {
     bestBeforeIndex?: number;
     useBy: Date;
 }
+
+export interface IRecipe {
+    title: string;
+    ingredients: string[];
+    bestBeforeIndex: number;
+    pastBestBefore: boolean;
+}
+
+type KitchenIngredients = {
+    useful: { [key: string]: I_Ingredient };
+    expired: I_Ingredient[];
+};
+
+type KitchenRecipes = {
+    available: IRecipe[];
+    nonAvailable: IRecipe[];
+};
+
 class Kitchen {
+    ingredients: KitchenIngredients;
+    recipes: KitchenRecipes;
+
     static loadIngredients(ingredients: I_Ingredient[], now: Date) {
         const useful: { [key: string]: I_Ingredient } = {};
         const expired: I_Ingredient[] = [];
@@ -24,4 +45,74 @@ class Kitchen {
         return { useful, expired };
     }
 
+    static prepareRecipes(
+        kitchenIngredients: KitchenIngredients,
+        recipes: IRecipe[]
+    ) {
+        const available: IRecipe[] = [];
+        const nonAvailable: IRecipe[] = [];
+
+        recipes.forEach((recipeItem) => {
+            const ingredients = recipeItem.ingredients;
+            let ingredientsBestBeforeIndex = 0; // sum(...bestBeforeIndex of ingredients)
+
+            for (let item of ingredients) {
+                const itemDetails = kitchenIngredients.useful[item];
+                if (!itemDetails) {
+                    // An ingredient is missing we cannot work with this recipe
+                    nonAvailable.push(recipeItem);
+                    return;
+                } else {
+                    if (itemDetails.bestBeforeIndex == undefined) {
+                        throw new Error(
+                            `failed to prepare recipes: bestBefore index for ${item} has not been calculated`
+                        );
+                    }
+                    if (itemDetails.bestBeforeIndex < 0) {
+                        recipeItem.pastBestBefore = true;
+                    }
+                    ingredientsBestBeforeIndex += itemDetails.bestBeforeIndex;
+                }
+            }
+
+            recipeItem.bestBeforeIndex = ingredientsBestBeforeIndex;
+            available.push(recipeItem);
+        });
+
+        return { available, nonAvailable };
+    }
+
+    constructor(ingredients: I_Ingredient[], recipes: IRecipe[], now: Date) {
+        this.ingredients = Kitchen.loadIngredients(ingredients, now);
+        this.recipes = Kitchen.prepareRecipes(this.ingredients, recipes);
+        this.recipes.available.sort((a, b) => {
+            if (a.pastBestBefore === true && b.pastBestBefore === true) {
+                const aIndex = a.bestBeforeIndex;
+                const bIndex = b.bestBeforeIndex;
+                if (aIndex < bIndex) {
+                    return 1;
+                }
+                if (aIndex > bIndex) {
+                    return -1;
+                }
+                return 0;
+            }
+
+            if (a.pastBestBefore === true && b.pastBestBefore === false) {
+                return 1;
+            }
+
+            if (a.pastBestBefore === false && b.pastBestBefore === true) {
+                return -1;
+            }
+
+            return 0;
+        });
+    }
+
+    recommendARecipe() {
+        return this.recipes;
+    }
 }
+
+export default Kitchen;
